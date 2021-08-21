6cc589004c1c04e9c2825670c6f18bbba52e32be
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-08-21 01:16:26 -0300

    Testande

--- src/test/java/zucc/api/controllers/NumberConversionTest.java
+++ src/test/java/zucc/api/controllers/NumberConversionTest.java
@@ -1,23 +1,19 @@
 package zucc.api.controllers;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
  * Class Description.
  *
  * @author Antônio Lima Jr
  */
-class NumberConversionTest {
-
-  public NumberConversionTest() {
-  }
-
-  @Test
-  void test() {
-    NumberConversion numberConversion = new NumberConversion();
-
-    assertEquals("Ola mundo", numberConversion.conversionNumber());
-  }
-}
+//class NumberConversionTest {
+//
+//  public NumberConversionTest() {
+//  }
+//
+//  @Test
+//  void test() {
+//    NumberConversion numberConversion = new NumberConversion();
+//
+//    assertEquals("Ola mundo", numberConversion.conversionNumber());
+//  }
+//}
