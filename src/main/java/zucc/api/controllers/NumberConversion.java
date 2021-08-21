60f7712439f9205a7732e4da2aa6e5a372aa236d
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-08-21 00:11:00 -0300

    outro teste

--- src/main/java/zucc/api/controllers/NumberConversion.java
+++ src/main/java/zucc/api/controllers/NumberConversion.java
@@ -1,12 +1,9 @@
 package zucc.api.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import zucc.api.dtos.Extenso;
-import zucc.api.services.NumberToString;
 
 /**
  * End-Point para a conversão do número por extenso.
@@ -16,11 +13,8 @@ import zucc.api.services.NumberToString;
 @RestController
 public class NumberConversion {
 
-  @Autowired
-  private NumberToString numberToString;
-
   @GetMapping("/{number}")
-  public ResponseEntity<Extenso> ConversionNumber(@PathVariable Integer number) {
-    return ResponseEntity.ok(numberToString.conversionNumber(number));
+  public ResponseEntity<String> ConversionNumber(@PathVariable Integer number) {
+    return ResponseEntity.ok("Ola mundo");
   }
 }
