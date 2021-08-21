e101ee861a494f9bc2a761bff3716418446ce13a
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-08-21 00:18:23 -0300

    Outro test

---
+++ src/test/java/zucc/api/controllers/NumberConversionTest.java
@@ -0,0 +1,12 @@
+package zucc.api.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Class Description.
+ *
+ * @author Antônio Lima Jr
+ */
+class NumberConversionTest {
+
+}
