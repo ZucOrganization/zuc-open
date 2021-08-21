132f77183f33b1bf81ff387ef7047143fb715ecf
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-08-21 01:05:29 -0300

    Testando

--- src/test/java/zucc/api/ApiApplicationTests.java
+++ src/test/java/zucc/api/ApiApplicationTests.java
@@ -3,11 +3,13 @@ package zucc.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  public void contextLoads() {
+    assertEquals("teste", "teste");
+  }
 }
